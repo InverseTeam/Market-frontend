e57bfdf2bfeb235e41cda40758f3a32ed5805a2b
Dmitriy <dimastepanov.1995@mail.ru>
2023-10-02 22:14:27 +0500

    change config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: { unoptimized: true },
-    output: 'standalone',
+    output: 'export ',
 };
 
 module.exports = nextConfig;
