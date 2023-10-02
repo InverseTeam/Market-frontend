a6cc3b121845e8ab667930d7a18a1c9c42b91c56
Dmitriy <dimastepanov.1995@mail.ru>
2023-10-02 22:16:04 +0500

    test params ig get quary

--- src/entities/productCard/ui/ui.tsx
+++ src/entities/productCard/ui/ui.tsx
@@ -22,7 +22,7 @@ export const ProductCard = () => {
         };
         getEvent();
     }, []);
-
+    console.log(productData);
     const skeletonMap = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
         <>
