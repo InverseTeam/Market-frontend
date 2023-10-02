00338499aef721b2baa5c0a802e4672ebf8a26da
Dmitriy <dimastepanov.1995@mail.ru>
2023-10-02 22:31:25 +0500

    fix bags

--- src/entities/productCard/model/index.ts
+++ src/entities/productCard/model/index.ts
@@ -2,7 +2,7 @@ import { instanceLogged } from '@/shared/api/axios';
 
 export const Get = async () => {
     try {
-        const getEvent: any = await fetch(`https://market.inverse-team.store/api/users/by?id=1`);
+        const getEvent: any = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=10`);
         const data = await getEvent.json();
         //instanceLogged.get('events/not_published/');
         return data;
